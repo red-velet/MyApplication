72a810f8384eaae7fdeb16a99ab3b6fe318b91d8
red-velet <redvelet@qq.com>
2024-01-23 14:17:27 +0800

    android学习：简单控件（文字、布局、按钮、图像、视图）

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("chiou","hello world");
+        Log.d("chiou", "hello world");
         setContentView(R.layout.activity_main);
         TextView customView = findViewById(R.id.tv);
         customView.setText("食肉何曾尽虎头，卅年书剑海天秋。\n" +
@@ -23,5 +25,49 @@ public class MainActivity extends AppCompatActivity {
                 "自是汝才难用世，岂真吾相不当侯？\n" +
                 "\n" +
                 "须知少日拏云志，曾许人间第一流");
+        //获取按钮，添加点击事件
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SongActivity.class);
+                startActivity(intent);
+            }
+        });
+        toTextContent(R.id.btn2TextContent);
+        toMyPage(R.id.btn2TextSize, TextSize.class);
+        toMyPage(R.id.btn2TextColor, TextColor.class);
+        toMyPage(R.id.btn2ViewBorder, ViewBorder.class);
+        toMyPage(R.id.btn2LinearLayout, LinearLayout.class);
+        toMyPage(R.id.btn2RelativeLayout, RelativeLayout.class);
+        toMyPage(R.id.btn2GridLayout, GridLayout.class);
+        toMyPage(R.id.btn2ScrollViewLayout, ScrollViewLayout.class);
+        toMyPage(R.id.btn2Button, MyButton.class);
+        toMyPage(R.id.btn2Image, Image.class);
+    }
+
+    public void toTextContent(int id) {
+        Button btn = findViewById(id);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TextContent.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void toMyPage(int id, Class<?> clazz) {
+        Button btn = findViewById(id);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, clazz);
+                startActivity(intent);
+            }
+        });
     }
 }
